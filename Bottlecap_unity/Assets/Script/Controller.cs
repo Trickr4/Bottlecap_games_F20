c01abcb5829406c335cb1e3f13b26002fef575b6
Aubtin Samai <aubtins@gmail.com>
2020-12-09 19:32:27 -0800

    Added Player Rotation
    Added player rotation with camera follow.

--- Bottlecap_unity/Assets/Script/Controller.cs
+++ Bottlecap_unity/Assets/Script/Controller.cs
@@ -2,26 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Controller : MonoBehaviour
 {
+    public CharacterController controller;
     private Rigidbody rb;
     public float speed;
+    public float rotationSpeed;
 
     void Start()
     {
-
+        controller = GetComponent<CharacterController>();
     }
+
     void Update()
     {
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime, 0);
 
+        Vector3 move = new Vector3(0, 0, Input.GetAxisRaw("Vertical") * Time.deltaTime);
+        move = transform.TransformDirection(move);
+        controller.Move(move * speed);
     }
-    void FixedUpdate()
-    {
-        float moveXAxis = Input.GetAxis("Horizontal");
-        float moveZAxis = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3(moveXAxis, 0, moveZAxis);
-        movement = Vector3.ClampMagnitude(movement, 1f);
-        transform.Translate(movement * speed * Time.deltaTime);
-    }
-
 }
