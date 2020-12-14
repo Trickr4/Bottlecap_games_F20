884cfb23cdc39c78c8af10e1a3a66a7f391b9639
Aubtin Samai <aubtins@gmail.com>
2020-12-13 21:37:15 -0800

    Enemy Health Bar
    Does not follow the camera currently.

---
+++ Bottlecap_unity/Assets/Script/HealthBar.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image healthBar;
+    
+    private Character _enemy;
+    private float _maxHealth;
+    
+    void Start()
+    {
+        _enemy = transform.parent.GetComponent<Character>();
+        _maxHealth = _enemy.hp;
+    }
+
+    void Update()
+    {
+        // Rotate the canvas to face the player.
+        // Not fully working.
+        // healthBar.transform.LookAt(Camera.main.transform);
+        
+        // Set the health amount.
+        healthBar.fillAmount = _enemy.hp / _maxHealth;
+    }
+}
